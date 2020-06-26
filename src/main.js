d962cfeb6cba77a1be407d2a78a0f49427a36dad
joannajjliu <joanna.liu3774@gmail.com>
2020-06-25 23:50:04 -0400

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './../node_modules/bulma/css/bulma.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false //app is in dev mode
 
+// new Vue instance
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App) //app rendered with App template
+}).$mount('#app') //mounted to DOM element with id being app
--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,21 @@ Vue.use(VueRouter)
   const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/event/:id',
+    name: 'eventSingle',
+    component: () => import('../views/EventSingle.vue')
   }
 ]
 
